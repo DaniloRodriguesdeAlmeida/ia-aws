bcdc3c17e6c3ed6bbce1159c8007a94f7f08ca3e
DaniloRodriguesdeAlmeida <danilo.mengo23@hotmail.com>
2026-01-07 21:35:30 -0300

    1 aula de Python

--- aula1.py
+++ aula1.py
@@ -1,7 +1,22 @@
-# Aula 01 
+tentativas = 0
+max_tentativas = 3
 
-# Criando minha primeira variavel
-fruta_citrica = "laranja"
+while tentativas < max_tentativas:
+    try:
+        num1 = float(input("digite o primeiro número: "))
+        num2 = float(input("digite o segundo número: "))
 
-# Imprimindo minha variavel
-print(fruta_citrica)
+        resultado = num1 / num2
+        print("resultado da divisão", resultado)
+        break #sai do loop se der certo
+    except ValueError:
+        tentativas += 1
+        print(f"Erro: digite apenas números. Tentativa {tentativas}/3\n")
+
+    except ZeroDivisionError:
+        tentativas += 1
+        print(f"Erro: não é impossivel dividir por zero. Tentativa {tentativas}/3\n")
+
+    finally:
+        if tentativas == max_tentativas:
+            print("número máximo de tentativas atigindo. Encerrando O programa")
