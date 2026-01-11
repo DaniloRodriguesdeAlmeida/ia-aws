b08895734a32c4319c87ec9c3b8fe83106d86d3e
DaniloRodriguesdeAlmeida <danilo.mengo23@hotmail.com>
2026-01-11 18:37:23 -0300

    Minha primeira atividade de Python

---
+++ atvpratica.py
@@ -0,0 +1,22 @@
+print("Olá, mundo!")
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+print("A soma é:", soma)
+comprimento = 12
+largura = 14
+altura = 20
+volume = comprimento * largura * altura
+print(f"O volume da caixa é: {volume} cm³")
+produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+preco_total = preco_unitario * quantidade
+
+print("--- Resumo da Compra ---")
+print(f"Produto: {produto}")
+print(f"Quantidade: {quantidade}")
+print(f"Preço Unitário: R$ {preco_unitario:.2f}")
+print(f"Total a pagar: R$ {preco_total:.2f}")
+
+
